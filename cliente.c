55d9df33a5aeb64393d162d3c67f2d32089e3d25
bundasmanu <gustavo.abcdef@hotmail.com>
2017-07-26 20:03:15 +0100

    Add files via upload

---
+++ cliente.c
@@ -0,0 +1,170 @@
+#include "utils.h"
+
+//variavel global que contem o pid associado ao cliente em questao
+int pid_cli;
+int fds_serv,desc_cliente;//descritores
+user k;
+int x;
+
+void reencaminha(){
+    printf("\nCheguei aqui.\n");
+    read(desc_cliente,&k,sizeof(user));//nao sei se vai chegar aqui bem
+    if(k.rr.logado==1){
+        printf("\nLogin validado.\n");
+    }
+    else{
+        printf("\nNao Validado.\n");
+    }
+return;
+}
+
+
+void ativar_sinal_sigusr2(){
+union sigval mysigval;
+
+        //envio do sinal ao servidor
+        sigqueue(x,SIGUSR2,mysigval);
+        if(strcmp(k.comando,"exit")==0){
+            return;//se o comando for exit nao vamos para a funcao reencaminha pois nao vamos ler nada do servidor terminamos logo
+        }
+        //este read nao sei se irá fazer
+        reencaminha();
+return;
+}
+
+void recebi_sinal_do_servidor(int signo, siginfo_t *info, void *extra){
+//depois é aqui que deve ser feita a validacao para mostrar o campo
+//vou ter de usar a estrutura sigval com o int-->para dizer quais as tarefas a serem feitas se é mostrar o jogo, ou avisar os clientes que o servidor terminou, etc
+
+//verificacao do valor que foi passado, no envio do sinal, por parte do servidor
+int verifica=0;
+verifica=info->si_value.sival_int;//-->contem o inteiro passado no envio do sinal
+
+    if(verifica==1){
+        fprintf(stderr,"\nRecebido sinal--> vai ser dado inicio ao jogo.\n");
+        /*
+        Aqui tera de ser desenhado o campo do jogo
+        */
+    }
+    if(verifica==2){
+        fprintf(stderr,"\nRecebido sinal-->O servidor vai terminar.\n");
+        return;
+    }
+return;
+}
+
+int verifica_estado_servidor(){
+
+if(access(FIFO_SERVIDOR,F_OK)!=0){
+    //o servidor nao ta a trabalhar
+    return 0;
+}
+return 1;
+}
+
+
+int main(int argc, char *argv[]){
+char nome_fifo_cliente[50];
+int res2, result_res;
+int ler_pid_serv;
+int verif_est_serv;
+
+
+//preparacao do sinal SIGUSR2, que vai ser utilizado para enviar os pareceres de login ao servidor
+struct sigaction sa;
+sa.sa_flags=SA_SIGINFO;//para ter os tres argumentos na funcao relativa ao sinal
+sa.sa_handler=SIG_DFL;//utilizar o default nao o ignorar
+sa.sa_sigaction=&recebi_sinal_do_servidor;
+sigemptyset(&sa.sa_mask);//inicializacao do sinal
+sigaction(SIGUSR2,&sa,NULL);
+
+//obtencao do PID do servidor, necessario para enviar os sinais
+ler_pid_serv=open("pid_servidor.txt",O_RDONLY);//tenho de melhorar isto, decerto é melhor criar uma estrutura com o pid do servidor
+//é so ler um numero mais nada
+read(ler_pid_serv,&x,sizeof(int));
+//pronto já tenho o pid do servidor, ja lhe posso enviar aquilo que pretendo
+
+
+    pid_cli=getpid();//aqui esta inerente o pid deste cliente
+    printf("\nPid do Cliente: %d.\n",pid_cli);
+    //colocar o pid no %d do FIFO do cliente
+    sprintf(nome_fifo_cliente,FIFO_CLIENTE,pid_cli);
+
+    //criação do fifo relativo ao cliente, agora já com o pid do cliente inerente
+    res2=mkfifo(nome_fifo_cliente,0777);
+    if(res2==-1){
+        perror("\nErro ao criar o FIFO relativo ao cliente.\n");
+        exit(EXIT_FAILURE);
+    }
+    //correu bem a criação do FIFO relativo ao cliente
+    fprintf(stderr,"\nCriado o fifo do cliente.\n");
+
+    //abertura do FIFO do cliente
+    desc_cliente=open(nome_fifo_cliente,O_RDWR);
+    if(desc_cliente==-1){
+        perror("\nErro na abertura do cliente, então vou desligar nao faz sentido continuar.\n");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr,"\nAbertura com sucesso do FIFO do cliente.\n");
+
+    //associacao com o servidor
+    fds_serv=open(FIFO_SERVIDOR,O_WRONLY);
+    if(fds_serv==-1){
+        perror("\nO servidor nao se encontra ligado, logo vou desligar.\n");
+        close(desc_cliente);
+        unlink(nome_fifo_cliente);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr,"\nCliente já se encontra associado ao servidor.\n");
+
+
+    while(1){
+
+
+        printf("<\n");
+        scanf(" %[^\n]",k.comando);
+                if(strcmp(k.comando,"login")==0){
+                                printf("\nIntroduza o seu user.\n");
+                                scanf(" %[^\n]",k.nome);
+                                printf("\nIntroduza a sua pass.\n");
+                                scanf(" %[^\n]",k.pass);
+                                //definicao de outros aspectos importantes, que sao importantes serem reconhecidos pelo servidor
+                                k.rr.logado=0;//esta a 0 inicialmente, porque ainda nao foi validado o seu login
+                                k.c.pids=pid_cli;//pid do cliente
+                                printf("\nLogado: %d\tPid: %d\n",k.rr.logado,k.c.pids);
+                                //envio de dados ao servidor, com user e pass do cliente
+                                write(fds_serv,&k,sizeof(user));
+                                printf("\nEnvio do sinal, com a info.\n");
+                                ativar_sinal_sigusr2();
+                                //reencaminha();
+                }
+                if(strcmp(k.comando,"exit")==0){
+                    //quando vou embora tenho de enviar um sinal ao servidor para colocar o logado deste cliente a 0
+                    //Resolver problema de quando vai um gajo embora meter o indice onde as estruturas nao tenham nada
+                    //quando for embora procuro o pid do gajo e meto a estrutura dele a 0-->memset
+                    //posso utilizar sempre a mesma estrutura, visto que ela apenas vai sendo atualizado no campo logado
+                  verif_est_serv=verifica_estado_servidor();
+                     if(verif_est_serv==1){//-->caso normal: o servidor ainda corre
+                        write(fds_serv,&k,sizeof(user));
+                        ativar_sinal_sigusr2();//-->envio a notificacao ao servidor com o pid e dps este retira-o do campo "logados"
+                        printf("\n\nOkOk Vou embora\n");
+                        close(desc_cliente);
+                        unlink(nome_fifo_cliente);
+                        exit(EXIT_SUCCESS);
+                     }
+                     if(verif_est_serv==0){
+                        close(desc_cliente);
+                        unlink(nome_fifo_cliente);
+                        exit(EXIT_SUCCESS);
+                     }
+                }
+            memset(k.comando,'\0',sizeof(k.comando));
+    }
+
+
+close(fds_serv);
+close(desc_cliente);
+unlink(nome_fifo_cliente);
+
+return 0;
+}
---
+++ servidor.c
@@ -0,0 +1,388 @@
+#include "utils.h"
+
+user w;
+userr v_din=NULL;//para ver isso dos logados-->comando "users"
+userr novo_din=NULL;
+int t=0;//-->controla insercoes
+int tt=0;//-->controla os exit's
+int desc_serv, desc_cli;//Descritores
+int logs;
+char *fich;
+char fifo_pid_cli[50];
+int result_res2;
+char comando[3][50]={"","",""};//necessario para o user username password
+int pid_servidor;//-->pid do servidor
+
+int verifica_login(char *nomefich){
+FILE *fr;
+char name[50],passwd[50];
+
+fr=fopen(nomefich,"r");
+    if(fr==NULL){
+        perror("\nErro ao tentar abrir o ficheiro.\n");
+        close(desc_serv);
+        unlink(FIFO_SERVIDOR);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr,"\nAberto o Ficheiro, estamos a validar.\n");
+
+    while(fscanf(fr,"%s\t%s",name,passwd)==2){
+            if(strcmp(w.nome,name)==0){
+                if(strcmp(w.pass,passwd)==0){
+                        //w.rr->logado=1;//colocado no ponteiro o estado do utilizador a 1
+                        return 1;
+                }
+            }
+            //senao encontrar o nome
+            fscanf(fr,"\n");//faço o \n que necessito
+    }
+fclose(fr);
+return 0;
+}
+
+void coloca_pid_servidor_fich(char *nomefich){
+int fich_serv;
+pid_servidor=getpid();
+
+    fich_serv=open(nomefich,O_CREAT | O_WRONLY,0777);
+    if(fich_serv==-1){
+        exit(EXIT_FAILURE);
+    }
+    //nao vou fazer prints, para nao sobrecarregar de texto a consola
+
+    write(fich_serv,&pid_servidor,sizeof(int));
+close(fich_serv);
+
+return;
+}
+
+int coloca_user_ficheiro(char *nomefich, char *mat[50]){
+FILE *fr;
+char user[50],pass[50];
+
+    printf("\nEntrei.\n");
+    printf("\nMat1: %s Mat2: %s\n",mat[1],mat[2]);
+
+    fr=fopen(nomefich,"a+");//colocar sempre no fundo
+    if(fr==NULL){
+        perror("\nErro ao abrir o ficheiro do usuarios.\n");
+        return 0;
+    }
+    //else
+    while(fscanf(fr,"%s\t%s\n",user,pass)==2){
+        //caso já existe o user associado
+        if(strcmp(user,mat[1])==0){
+            fprintf(stderr,"\nJá existe esse user, altere o nome do seu user.\n");
+            return 0;
+        }
+        //caso nao exista user associado,então
+        if(strcmp(user,mat[1])!=0 && feof(fr)){//se nao encontrar e chegar ao fim do ficheiro entao agora posso escrever o user no ficheiro
+            fprintf(fr,"%s\t%s\n",mat[1],mat[2]);
+            fprintf(stderr,"\nUser Colocado no Ficheiro.\n");
+            fclose(fr);//necessario o fclose
+            return 1;
+        }
+    }
+fclose(fr);
+return 0;
+}
+
+
+
+void recebi_sinal(int signo, siginfo_t *info, void *extra){
+int i=0,kk=0;//variavel que permite controlar o numero de users ate chegar a 20-->usado no comando "users"
+
+switch(signo){
+
+    case SIGUSR2:
+
+    printf("\nt: %d\n",t);
+    printf("\ntt: %d\n",tt);
+
+    printf("\nRecebi pedido por parte de um cliente.\n");
+    result_res2=read(desc_serv,&w,sizeof(user));
+    if(result_res2 < sizeof(user)){
+            fprintf(stderr,"\nResposta nao foi recebida na integra.\n");
+            return;
+    }
+    fprintf(stderr,"\nPedido de Validacao entregue, vamos verificar.\n");
+    if(strcmp(w.comando,"login")==0){
+        logs=verifica_login("users.txt");
+            if(logs==1){//Se existe
+                    //envio da resposta ao cliente
+                    //necessario saber o pid do cliente, que esta inserido na estutura do usuario
+                    //basta enviar a verificar, no lado do cliente é que vai existir a validacao (se encontrou ou nao)
+                    //agora vou colocar o pid no FIFO_SERVIDOR, e abri-lo para enviar a info ao cliente com aquele pid
+                    printf("\nPid relativo ao cliente que enviou a mensagem: %d",w.c.pids);
+                    fflush(stdout);
+                    sprintf(fifo_pid_cli,FIFO_CLIENTE,w.c.pids);
+                    //abro o cliente pa escrita
+                    desc_cli=open(fifo_pid_cli,O_WRONLY);
+                    if(desc_cli==-1){
+                        perror("\nErro ao abrir o cliente.\n");
+                    }
+                    else{
+                        fprintf(stderr,"\nAberto FIFO do cliente pa escrita.\n");
+
+                        w.rr.logado=1;//já se encontra logado e dps envio
+                        t++;//variavel aumenta-->esta variavel é responsavel por fazer o realloc
+
+                        if(t==1){
+                            //alocacao de memoria para definir novo user logado
+                            v_din = (userr) malloc(sizeof(user)*1);
+                        }
+
+                        //já tenho a alocacao de memoria feita, é feita no inicio do programa, agora tenho de inserir os users logados
+                     for(i=0;i<t;i++){
+                        if(v_din[i].rr.logado!=1 && kk==0){//colocar apenas 1 vez(atraves do kk), na 1 posicao que encontrar o user logado
+                            v_din[i].c.pids=w.c.pids;
+                            v_din[i].rr.logado=1;
+                            kk++;
+                            printf("\nPosicao: %d",i);
+                        }
+                     }
+
+                     //aloco logo espaco para mais uma nova entrada depois da insercao de uma pessoa
+                     novo_din=realloc(v_din,1+t);
+                     v_din=novo_din;
+
+                        //ate chegar ao limite de 16 users logados-->para já nao tou a validar isso
+                        //escreve na estrutura elementar o logado que depois é passado ao cliente-->no write
+                        write(desc_cli,&w,sizeof(user));
+
+                        close(desc_cli);//fecho logo o descritor do cliente, dps de enviada a informacao
+                        return;
+                    }
+            }
+            else{//se nao coincide
+                    sprintf(fifo_pid_cli,FIFO_CLIENTE,w.c.pids);
+                    //abro o cliente pa escrita
+                    desc_cli=open(fifo_pid_cli,O_WRONLY);
+                    if(desc_cli==-1){
+                        perror("\nErro ao abrir o cliente.\n");
+                    }
+                    fprintf(stderr,"\nAberto FIFO do cliente pa escrita.\n");
+
+                    w.rr.logado=0;
+
+
+                    write(desc_cli,&w,sizeof(user));
+
+                    close(desc_cli);//fecho logo o descritor do cliente, dps de enviada a informacao
+                    return;
+
+            }
+    }
+    if(strcmp(w.comando,"exit")==0){
+        //agora o que tenho de fazer é percorrer todos os clientes logados e ver onde ta aquele com o pid enviado
+        //para esse cliente-->retiro-o dos logados-->ou atraves do memset ou outra forma qualquer
+        for(i=0;i<t;i++){
+            if(v_din[i].c.pids==w.c.pids){
+                    //aloco logo espaco para mais uma nova entrada para apagar uma pessoa
+                     novo_din=realloc(v_din,1+t);
+                     v_din=novo_din;
+                if((t-tt)!=1){
+                    v_din[i]=v_din[i+1];
+                    printf("\nEntrei diferente.\n");
+                }
+                if((t-tt)==1){
+                    //alocacao de memoria para definir novo user logado
+                    printf("\nEntrei igual.\n");
+                    v_din = (userr) malloc(sizeof(user)*1);
+                }
+                //apaga-lo do panorama
+                printf("\nEntrei na eliminacao do usuario.\n");
+                tt++;//-->tem de se atualizar no fim de apagar para dar certo
+        }
+
+    }
+    }
+    break;
+
+case SIGUSR1:
+
+    //fazer aqui implementacao do encerramento
+    close(desc_serv);
+    unlink(FIFO_SERVIDOR);
+    exit(EXIT_SUCCESS);
+
+break;
+
+}//fecha switch
+
+}
+
+void envia_threads_user(){
+
+
+}
+
+void verifica_logados(){
+    int i;
+
+printf("\nLogados:\n");
+    for(i=0;i<t-tt;i++){//definir tamanho de jogadores-->os que inseri - apaguei = tamanho dos users
+        //percorrer todos os jogadores
+        if(v_din[i].rr.logado==1){
+            fprintf(stderr,"\nPid: %d\n",v_din[i].c.pids);
+        }
+
+    }
+
+return;
+}
+
+void envia_sinal_entrar_jogo(userr n){//recebe o vetor dinamico-->funcao que tem como objectivo mostrar o jogo aos clientes
+int i;
+union sigval sig;
+sig.sival_int=1;//->quando o cliente receber o sinal vai verificar o inteiro que foi passado como parametro
+
+//falta definir os inteiros nesta funcao e no cliente  verificar se da uniao recebeu 1 ou 2 e agir conforme o pedido
+
+    for(i=0;i<t-tt;i++){
+            sigqueue(n[i].c.pids,SIGUSR2,sig);
+    }
+return;
+}
+
+void envia_sinal_sair_jogo_aos_clis(userr nn){//funcao que diz aos clientes que o servidor vai terminar
+//percorrer o vetor dinamico e enviar sinal a todos os users logados
+int i;
+union sigval sig;
+sig.sival_int=2;//-->sair do jogo tem associado
+
+//falta definir os inteiros nesta funcao
+
+    for(i=0;i<t-tt;i++){
+        sigqueue(nn[i].c.pids,SIGUSR2,sig);
+    }
+
+
+return;
+}
+
+void envia_sigusr1_terminar(){
+union sigval sig;
+
+    sigqueue(pid_servidor,SIGUSR1,sig);
+
+return;
+}
+
+void define_var_ambiente(){
+char *dd,*ee;
+int NDEFESAS,NAVANCADOS;
+
+        dd=getenv("NDEFESAS");
+        if(dd==NULL){
+            NDEFESAS=2;
+        }
+        else{
+            NDEFESAS=atoi(dd);
+            printf("NDEFESAS: %d\n",NDEFESAS);
+        }
+
+        ee=getenv("NAVANCADOS");
+        if(ee==NULL){
+            NAVANCADOS=2;
+        }
+        else{
+            NAVANCADOS=atoi(ee);
+            printf("NAVANCADOS: %d\n",NAVANCADOS);
+        }
+
+return;
+}
+
+
+int main(int argc, char *argv[]){
+int res;
+char pedido[50];
+char *comando2[50];//necessario para o user username password
+char comando[50]="";
+int k;
+char *token;
+
+//verificacao se já existe um servidor aberto, se existir fecha este novo
+if(access(FIFO_SERVIDOR,F_OK)==0){
+    fprintf(stderr,"\nO servidor já se encontra a ser executado.\n");
+    exit(EXIT_SUCCESS);
+}
+
+
+define_var_ambiente();
+
+fich=argv[1];
+
+    //implementacao da estrutura do signal recebido no login
+    struct sigaction sas;
+    sas.sa_flags=SA_SIGINFO;
+    sigemptyset(&sas.sa_mask);//inicializacao da estrutura
+    sas.sa_handler=SIG_DFL;//modificacao apos
+    sas.sa_sigaction= &recebi_sinal;
+    sigaction(SIGUSR2,&sas,NULL);
+    sigaction(SIGUSR1,&sas,NULL);
+
+    coloca_pid_servidor_fich("pid_servidor.txt");
+
+    res=mkfifo(FIFO_SERVIDOR,0777);//dadas todas as permissoes
+    if(res==-1){
+        perror("\nErro ao criar o fifo do servidor.\n");
+        exit(EXIT_FAILURE);
+    }
+    //se correu bem
+    fprintf(stderr,"\nO fifo do servidor foi criado.\n");
+
+
+    desc_serv=open(FIFO_SERVIDOR,O_RDWR);
+    if(desc_serv==-1){
+        perror("\nErro ao abrir o fifo do servidor.\n");
+        //como nao consegui abrir, não faz sentido continuar no programa
+        exit(EXIT_FAILURE);
+    }
+    //se consegui abrir o FIFO_SERVIDOR
+    fprintf(stderr,"\nAberto o FIFO do servidor.\n");
+
+
+    while(1){
+    int x=0;
+        printf("\n<\n");
+        scanf(" %[^\n]",comando);
+        //mudar isto aqui e utilizar o strtok-->ou seja no scanf utilizar uma string normal, e usando o strtok separo o que foi escrito e valido conforme o que foi escrito no 1 argumento user, users, etc
+        token=strtok(comando," ");
+                while(token!=NULL){
+                    printf("\n%s\n",token);
+                    comando2[x]=token;
+                    printf("\n%s\n",comando2[x]);
+                    x++;
+                    token=strtok(NULL," ");
+        }
+            if(strcmp(comando2[0],"user")==0){
+                //chamar uma funcao que verificar se um username já existe, se existir nao mete, se nao existir adiciona o user ao ficheiro
+                printf("\nEntrei aqui.\n");
+                k=coloca_user_ficheiro(argv[1],comando2);
+            }
+            if(strcmp(comando2[0],"users")==0){
+            printf("\nEntrei.\n");
+                verifica_logados();
+            }
+            if(strcmp(comando2[0],"start")==0){
+                envia_sinal_entrar_jogo(v_din);
+            }
+            if(strcmp(comando2[0],"shutdown")==0){
+                //envio do sinal aos cli's a dizer que vou embora
+                envia_sinal_sair_jogo_aos_clis(v_din);
+                //recebo sinal SIGUSR1 e termino a execução apagando o FIFO do servidor
+                envia_sigusr1_terminar();
+            }
+
+
+        memset(comando,' ',sizeof(comando));//reset aos comandos definidos pelo user para nao executar sempre o mesmo até que se volte a inserir novos comandos
+        //tenho de fazer memset do comando2
+    }
+free(v_din);
+close(desc_serv);
+unlink(FIFO_SERVIDOR);
+
+return 0;
+}
